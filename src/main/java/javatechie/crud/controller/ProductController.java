7ad69af7cdf2419d5554f320c106c48142da39e7
Kyara Araújo <araujomrk@gmail.com>
2021-09-28 14:21:18 -0300

    cria controller e metodos

---
+++ src/main/java/javatechie/crud/controller/ProductController.java
@@ -0,0 +1,50 @@
+package javatechie.crud.controller;
+
+import javatechie.crud.entity.Product;
+import javatechie.crud.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @PostMapping(path = "/addProduct")
+    public Product addProduct(@RequestBody Product product){
+        return service.saveProduct(product);
+    }
+
+    @PostMapping(path = "/addProducts")
+    public List<Product> addProducts(@RequestBody List<Product> products){
+        return service.saveProducts(products);
+    }
+
+    @GetMapping(path = "/products")
+    public List<Product> findAllProducts(){
+        return service.getProducts();
+    }
+
+    @GetMapping(path = "/products/{id}")
+    public Product findProductById(@PathVariable int id){
+        return service.getProductById(id);
+    }
+
+    @GetMapping(path = "/products/{name}")
+    public Product findProductByName(@PathVariable String name){
+        return service.getProductByName(name);
+    }
+
+    @PutMapping(path = "/update")
+    public Product updateProduct(@RequestBody Product product){
+        return service.updateProduct(product);
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public String deleteProduct(@PathVariable int id){
+        return service.deleteProduct(id);
+    }
+}
